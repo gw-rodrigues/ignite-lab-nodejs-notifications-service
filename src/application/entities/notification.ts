197d912a02033c51d1959c9b438c126501b97d82
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-04 10:39:49 +0100

    feat: create notification class

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,47 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
