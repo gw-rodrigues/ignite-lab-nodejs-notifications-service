c25a2071811894425b841fbe443f0a067f5c8ad6
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-05 11:14:19 +0100

    fix: folder and file paths

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,5 +1,5 @@
-import { Notification } from 'src/application/entities/notification';
-import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { Notification } from '../../src/application/entities/notification';
+import { NotificationsRepository } from '../../src/application/repositories/notifications-repository';
 
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
