c2fa1ae8be65b6f4c710681ab07911dd762684f9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-05 10:07:48 +0100

    build: moving folder

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,5 +1,5 @@
-import { Content } from 'src/application/entities/content';
-import { Notification } from 'src/application/entities/notification';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
 
 interface SendNotificationRequest {
   recipientId: string;
