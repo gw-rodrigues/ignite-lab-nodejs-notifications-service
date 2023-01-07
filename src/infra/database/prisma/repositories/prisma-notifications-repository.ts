387f75175adfff0d2cbedeb170db31cb6d6f963a
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-07 10:52:58 +0100

    feat: create prisma service method create data

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,20 @@
+import { Notification } from '../../../../application/entities/notification';
+import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        recipientId: notification.recipientId,
+        content: notification.content.value,
+        category: notification.category,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
