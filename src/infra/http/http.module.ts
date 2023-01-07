e46aa39b5b019d813061a8bebd44716c39e465db
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-07 10:53:24 +0100

    feat: add send notification providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
   imports: [],
   controllers: [NotificationsController],
+  providers: [SendNotification],
 })
 export class HttpModule {}
