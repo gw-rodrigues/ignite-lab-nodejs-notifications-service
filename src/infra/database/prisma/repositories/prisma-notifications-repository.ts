c9fe00c0354f0ba6c2d9209e6d235dc80db7b07a
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-07 11:03:58 +0100

    feat: add @injectable from nestjs

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { Notification } from '../../../../application/entities/notification';
 import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
 import { PrismaService } from '../prisma.service';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
