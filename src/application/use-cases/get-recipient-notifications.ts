e4c3679f64deb1897380c70e9942197b6d1b616d
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-10 18:25:06 +0100

    feat: create method find recipient notifications

---
+++ src/application/use-cases/get-recipient-notifications.ts
@@ -0,0 +1,28 @@
+import { Notification } from '@application/entities/notification';
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface GetRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationsResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: GetRecipientNotificationsRequest,
+  ): Promise<GetRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const notifications = await this.notificationRepository.findManyRecipientId(
+      recipientId,
+    );
+
+    return { notifications };
+  }
+}
