4e0393c2273a7473af30fff6799ec8284d7e24d6
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-10 17:37:55 +0100

    feat: add props de cancel and save notification

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -7,11 +7,25 @@ export class InMemoryNotificationsRepository
   public notifications: Notification[] = [];
 
   async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
   }
 
   async create(notification: Notification) {
