66881f1f59046a9cd4dc85b71d00240302d7e167
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-10 17:49:24 +0100

    feat: create count recipient notifications method

---
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface CountRecipientNotificationRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationRequest,
+  ): Promise<CountRecipientNotificationResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationRepository.countManyRecipientId(
+      recipientId,
+    );
+
+    return { count };
+  }
+}
