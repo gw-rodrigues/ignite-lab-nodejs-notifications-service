736c27e1844007fc2351a7050e8c4f466a2a4104
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-10 15:44:19 +0100

    feat: create mapper to return data

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,14 @@
+import { Notification } from '@application/entities/notification';
+
+export class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      recipientId: notification.recipientId,
+      content: notification.content.value,
+      category: notification.category,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+}
