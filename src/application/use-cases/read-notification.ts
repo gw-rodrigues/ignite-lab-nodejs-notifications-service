01be0139ada97372a0cf4cb57eca9288d0989ee7
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-10 18:36:06 +0100

    feat: read recipient notification method

---
+++ src/application/use-cases/read-notification.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface ReadNotificationRequest {
+  notificationId: string;
+}
+
+type ReadNotificationResponse = void;
+
+@Injectable()
+export class ReadNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: ReadNotificationRequest,
+  ): Promise<ReadNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.read();
+
+    await this.notificationRepository.save(notification);
+  }
+}
