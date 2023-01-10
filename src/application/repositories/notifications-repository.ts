f00459b886f86d2f777e69ccedd94fbf49a7da66
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-10 16:31:49 +0100

    feat: add method findById

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -2,4 +2,5 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
 }
