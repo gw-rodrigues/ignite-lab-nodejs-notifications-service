dac329fddc493a93f34a6a44b16b208aab24468b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-11 18:16:06 +0100

    feat: add kafka and start micro services

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +11,12 @@ async function bootstrap() {
   //if Post data/field is invalid the app send to the requester a error json* message
   app.useGlobalPipes(new ValidationPipe());
 
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService,
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
