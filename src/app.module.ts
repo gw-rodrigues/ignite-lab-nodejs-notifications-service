c5c94986fc514814d0d6182a432ad27b11d1a36a
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-11 15:49:48 +0100

    fix: messaging service path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
-import { MessagingModule } from '@infra/messaging/kafka/messaging.module';
+import { MessagingModule } from '@infra/messaging/messaging.module';
 
 @Module({
   imports: [HttpModule, DatabaseModule, MessagingModule],
